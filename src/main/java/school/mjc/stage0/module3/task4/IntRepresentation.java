9585c01202df9f4fa791e6e5a0b9440c9eb1c499
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-16 16:30:25 +0300

    fix: test issue

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,6 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        System.out.println((char)(99) + "" + (char)(104) + (char)(97) + (char)(114));
+        System.out.print((char)(99));
+        System.out.print((char)(104));
+        System.out.print((char)(97));
+        System.out.print((char)(114));
     }
 }
